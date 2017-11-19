867a3a7a558e346250720a1b86df109ac1c78e13
JACK <luckyjack85@gmail.com>
2017-11-20 02:35:44 +0800

    add getcomments

--- getcomments.js
+++ getcomments.js
@@ -1,9 +1,13 @@
 const https = require("https");
 
-var base = "https://www.googleapis.com/youtube/v3/videos?id=";
+var base = "https://www.googleapis.com/youtube/v3/commentThreads?videoId=";
 var key = "&key=AIzaSyClG1COBS6izq7X-_rTqMjp-N01GGNaMSA";
-var backpart = "&part=snippet,contentDetails,statistics,status";
-var videoarray = ["7BbonQ0OIQ4", "LPoZm0R2tog"];
+var backpart = "&part=snippet,replies";
+var videoarray = ["xuCQFhPanr4", "JHDF-SFvLFQ", "b7cMcbDgxnc", "N88K0MCL9Vc",
+                  "GiILtNnnU7M", "UX4Cp5NuKX4", "PCzssr56nkU", "9BMxY2GW5hU", "Nwi-YqrEhtg"];
+var people = ["菜阿嘎", "阿滴英文","上班不要看", "啾啾鞋", 
+              "走路痛", "HowHow", "林辰" , "星期天", "囧星人"]
+
 console.log(base + videoarray[1] + key + backpart)
 //https://stackoverflow.com/questions/4976466/difference-between-process-stdout-write-and-console-log-in-node-js
 //info link
@@ -25,12 +29,13 @@ https
     // res.on("end", forend("testing"));
     res.on('end', () => {
       var output = JSON.parse(dataQueue)
-      var basic = output.items[0].snippet
+      
+      var basic = output.items[0]
       console.log(output)
-      console.log("Uploader Name : "+ basic.channelTitle);
-      console.log("Uploadtime    : "+ basic.publishedAt)
-      console.log("Channel Title : "+ basic.localized.title);
-      console.log("Description   : "+ basic.localized.description);
+      // console.log("Uploader Name : "+ basic.channelTitle);
+      // console.log("Uploadtime    : "+ basic.publishedAt)
+      // console.log("Channel Title : "+ basic.localized.title);
+      // console.log("Description   : "+ basic.localized.description);
     })
   })
   .on("error", e => {
