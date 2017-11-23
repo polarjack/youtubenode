b03c0c8a597399268ed2b09717b81c6e5e66b5e8
polarjack <luckyjack85@gmail.com>
2017-11-23 10:06:49 +0800

    promise half

--- getcomments.js
+++ getcomments.js
@@ -1,16 +1,33 @@
 const https = require("https");
-const fs = require('fs')
-const sleep = require('sleep');
+const fs = require("fs");
+const sleep = require("sleep");
+const Promise = require("promise");
 
 var base = "https://www.googleapis.com/youtube/v3/commentThreads?videoId=";
 var key = "&key=AIzaSyClG1COBS6izq7X-_rTqMjp-N01GGNaMSA";
 var backpart = "&part=snippet,replies&maxResults=100";
-var videoarray = ["xuCQFhPanr4", "JHDF-SFvLFQ", "b7cMcbDgxnc", "N88K0MCL9Vc",
-  "GiILtNnnU7M", "UX4Cp5NuKX4", "PCzssr56nkU", "9BMxY2GW5hU", "Nwi-YqrEhtg"
+var videoarray = [
+  "xuCQFhPanr4",
+  "JHDF-SFvLFQ",
+  "b7cMcbDgxnc",
+  "N88K0MCL9Vc",
+  "GiILtNnnU7M",
+  "UX4Cp5NuKX4",
+  "PCzssr56nkU",
+  "9BMxY2GW5hU",
+  "Nwi-YqrEhtg"
 ];
-var people = ["菜阿嘎", "阿滴英文", "上班不要看", "啾啾鞋",
-  "走路痛", "HowHow", "林辰", "星期天", "囧星人"
-]
+var people = [
+  "菜阿嘎",
+  "阿滴英文",
+  "上班不要看",
+  "啾啾鞋",
+  "走路痛",
+  "HowHow",
+  "林辰",
+  "星期天",
+  "囧星人"
+];
 
 //https://stackoverflow.com/questions/4976466/difference-between-process-stdout-write-and-console-log-in-node-js
 //info link
@@ -33,26 +50,25 @@ function todo(url, name) {
       // process.stdout.write(dataQueue);
 
       // res.on("end", forend("testing"));
-      res.on('end', () => {
+      res.on("end", () => {
+        console.log(name);
+        var output = JSON.parse(dataQueue);
 
-        console.log(name)
-        var output = JSON.parse(dataQueue)
+        var basic = output.items[0];
 
-        var basic = output.items[0]
-
-        const tojson = JSON.stringify(output)
-        fs.writeFile('./'+ name + '.json', tojson, 'utf8', function(err) {
-          if(err) {
-            console.log(err)
+        const tojson = JSON.stringify(output);
+        fs.writeFile("./" + name + ".json", tojson, "utf8", function(err) {
+          if (err) {
+            console.log(err);
           }
-          console.log("file save")
-        })
+          console.log("file save");
+        });
         // console.log(output)
         // console.log("Uploader Name : "+ basic.channelTitle);
         // console.log("Uploadtime    : "+ basic.publishedAt)
         // console.log("Channel Title : "+ basic.localized.title);
         // console.log("Description   : "+ basic.localized.description);
-      })
+      });
     })
     .on("error", e => {
       console.error(e);
@@ -63,36 +79,63 @@ function getnextpage(url, token, counting) {
   //url => url + &pageToken=token
   // output.nexPageToken
 }
+var counting = 0;
+var i = 0;
+var inputtoken = "";
 
+function job() {
+  while (counting++ < 10) {
+    httpwithtoken(base + videoarray[i] + key + backpart, inputtoken, people[i]);
+    sleep.sleep(1)
+    console.log(counting);
+  }
 
+  console.log(counting);
+}
+function secondjob(token) {
+  var testing = httpwithtoken();
+}
 
-// var i = 0;
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
-// i++
-// todo(base + videoarray[i] + key + backpart, people[i])
-// sleep.sleep(1)
+job();
 
+function httpnew(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, res => {
+        var dataQueue = "";
+        res.on("data", d => {
+          dataQueue += d;
+        });
+        res.on("end", () => {
+          var output = JSON.parse(dataQueue);
+          return resolve(output);
+        });
+      })
+      .on("error", e => {
+        //console.error(e);
+        return reject(e);
+      });
+  });
+}
 
-// console.log(i)
+function httpwithtoken(url, token, name) {
+  var todo = token == "" ? url : url + "&pageToken=" + token;
+  console.log(todo);
+  // return new Promise((resolve, reject) => {
+  //   https
+  //     .get(todo, res => {
+  //       var dataQueue = "";
+  //       res.on("data", d => {
+  //         dataQueue += d;
+  //       });
+  //       res.on("end", () => {
+  //         var output = JSON.parse(dataQueue);
+  //         return resolve(output);
+  //       });
+  //     })
+  //     .on("error", e => {
+  //       //console.error(e);
+  //       return reject(e);
+  //     });
+  // });
+}
